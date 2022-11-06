4002541f1f88b8073704bf6e4ffe987e7e8f2b31
Michael Chop <007micle007@gmail.com>
2022-11-06 03:53:52 +0300

    Made the basic layout of the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {useAppSelector} from "./hooks/redux";
+
+function App() {
+
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
